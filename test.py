975bdec5ff710683c70ca587ab5a06c3edc387e0
kuihim <kuihim@qq.com>
2024-04-03 10:56:42 +0800

    conf change

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
 from datetime import datetime, timedelta
 import random
+import os
 
-files = []
-for i in range(20):
-    name = '5.html@' + (datetime.now() + timedelta(hours=random.randint(0, 100))).strftime('%Y-%m-%d_%H:%M:%S')
-    files.append(name)
-print(sorted(files, key=lambda y: y[-20:]))
+print(os.path.isdir('/srv/chen'))
