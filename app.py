a55d0e65c0a7ebe8d96e9000ff3dad7f647e62b8
kuihim <kuihim@qq.com>
2024-04-17 12:48:25 +0800

    add upload

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime
 import os
 import shutil
+import requests
 
 _5html_path = '/srv/chen77477/5.html'
 _5html_name = os.path.split(_5html_path)[1]
@@ -29,6 +30,7 @@ def get_file_md5(file_path):
     return str(_hash).upper()
 
 
+# 备份文件
 def dump_5html():
     # dump file 5.html_2020-01-01 12:00:00
     dump_name = _5html_name + '@' + datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
@@ -43,11 +45,29 @@ def dump_5html():
             os.remove(os.path.join(_dump_dir, f))
 
 
-if __name__ == '__main__':
+# 上传文件到备用服务器
+def upload_5html():
+    domain_list = ['b.delphi7.top', 'delphi7.top']
+    try:
+        for domain in domain_list:
+            upload_url = 'http://' + domain + '/5html/?check=mihiuk'
+            files = {'file': open(_5html_path, 'rb')}
+            requests.post(upload_url, files=files, timeout=5)
+    except Exception as e:
+        print(e)
+
+
+def main():
     pre_5html_md5 = get_file_md5(_5html_path)
     while True:
         cur_5html_md5 = get_file_md5(_5html_path)
         if cur_5html_md5 != pre_5html_md5:
             pre_5html_md5 = cur_5html_md5
             dump_5html()
-        time.sleep(10 * 60)
+            upload_5html()
+        time.sleep(2)
+
+
+if __name__ == '__main__':
+    # main()
+    upload_5html()
